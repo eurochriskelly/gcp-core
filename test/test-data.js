bc592304dd45fe53f330772a3959bf19e621cba2
eurochriskelly@gmail.com <eurochriskelly@gmail.com>
2024-03-30 19:04:48 +0100

    Move test data into common location

---
+++ test/test-data.js
@@ -0,0 +1,76 @@
+module.exports = {
+  tournaments: {
+    t1: {
+      tournamentId: 6,
+      description: "Benelux round A",
+      startDate: "2024-04-01",
+      pitches: ["Pitch 5", "Pitch 11", "Pitch 12", "Pitch 13"],
+      categories: {
+        Mens: {
+          g1: [
+            "Amsterdam A",
+            "Groningen",
+            "Leuven A",
+            "Hamburg/Bel'B'",
+            "Eindhoven",
+          ],
+          g2: ["Luxembourg A", "Nijmegen", "Hague A", "Dusseldorf/Cologne"],
+          g3: ["Belgium A", "Leuven B", "Maastricht", "Frankfurt"],
+        },
+        Ladies: {
+          g1: [
+            "Leuven A",
+            "A'dam'B'/Leuven'B'",
+            "Belgium A",
+            "Groningen",
+            "Nijmegen",
+          ],
+          g2: [
+            "Luxembourg A",
+            "Hague/Frankfurt",
+            "Hamb/Duss/Lux'B'",
+            "Amsterdam A",
+          ],
+        },
+      },
+      fixtures: [
+        [
+          1,
+          "10:30",
+          "Pitch 5",
+          "group",
+          "Mens",
+          1,
+          "Amsterdam A",
+          "Leuven A",
+          "Groningen",
+          15,
+        ],
+        [
+          2,
+          "11:00",
+          "Pitch 5",
+          "group",
+          "Mens",
+          3,
+          "Belgium B",
+          "Leuven B",
+          "Leuven A",
+          20,
+        ],
+        [
+          3,
+          "11:30",
+          "Pitch 5",
+          "group",
+          "Mens",
+          1,
+          "Leuven A",
+          "Groningen",
+          "Belgium A",
+          15,
+        ],
+      ],
+    },
+  },
+};
