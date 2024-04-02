19a05877047ff49b247ade865c477e4fb92bfdec
Chris Kelly <eurochriskelly@gmail.com>
2024-04-02 23:27:11 +0200

    Generate tourament until knockout stage

--- src/TournamentOrganize.class.js
+++ src/TournamentOrganize.class.js
@@ -7,7 +7,7 @@
  * - Calculating fixtures for the knockout stage for each cup in tournament
  * - Scheduling the tournament
  */
-const { assignTeamsToGroups } = require("./util");
+const { assignTeamsToGroups, calculateGroupStageFixtures } = require("./util");
 
 class TournamentOrganize {
   constructor(T) {
@@ -15,75 +15,67 @@ class TournamentOrganize {
     this.nextFixture = 1;
   }
 
+  /**
+   * Generate fixtures for the tournament by apply the following steps:
+   * 1. [x] Assign teams to groups
+   * 2. [x] Generate group stage fixtures (external function)
+   * 3. [x] Order group stage fixtures
+   * 4. [ ] Generate knockout stage fixtures
+   * 5. [ ] Order knockout stage fixtures
+   * 6. [ ] Assign upmires to fixtures
+   * 
+   */
   generate() {
     this.nextFixture = 1;
     // Assign teams to groups 
+    const T = this.tournament;
+    T.clearFixtures();
+    Object.keys(T.categories).forEach(cat => {
+      this.assignTeamsToGroups(cat, true);
+      T.fixtures = [
+        ...T.fixtures, 
+        // 2. Generate group stage fixtures
+        ...calculateGroupStageFixtures(
+          cat,
+          T.categories[cat].groups,
+          [0, 0, 60, 40, 25, 20, 15]
+        )
+      ]
+    });
+    this.orderGroupStageFixtures();
+    this.generateKnockoutFixtures();
+
+    //console.log(T)
+    console.table(T.fixtures);
     // for each category in categories generate group fixtures
   }
 
-  assignTeamsToGroups(category) {
+  // 1. Assign teams to groups
+  assignTeamsToGroups(category, randomize = false) {
     const T = this.tournament;
     const { teams } = T.categories[category];
-    T.categories[category].groups = assignTeamsToGroups(teams);
+    T.categories[category].groups = assignTeamsToGroups(teams, randomize);
   }
 
-  /**
-   * Generate matches for each group
-   * @returns
-   */
-  calculateGroupStageFixtures(category) {
+  generateKnockoutFixtures() {
     const T = this.tournament;
-    let matches = [];
-    // Group teams by their group
-    T.categories[category].groups.map((teams, i) => {
-      const group = i;
-      for (let i = 0; i < teams.length; i++) {
-        for (let j = i + 1; j < teams.length; j++) {
-          matches.push({
-            matchId: this.nextFixture++,
-            category,
-            group,
-            stage: 'group',
-            team1: teams[i],
-            team2: teams[j],
-          });
-        }
-      }
-    })
-/*
-    Object.keys(teamsByGroup).forEach((group) => {
-      const groupTeams = teamsByGroup[group];
-      let timeOffset = 0;
-      let nextStart = "09:00";
-      for (let i = 0; i < groupTeams.length; i++) {
-        for (let j = i + 1; j < groupTeams.length; j++) {
-          const d = this.stageParameters.find(
-            (d) => d.groupSize === this.groupSizes[group],
-          );
-          const { duration = 0, break: breakDuration = 0, gap = 0 } = d || {};
-          matches[group].push({
-            offset: timeOffset,
-            start: nextStart,
-            team1: groupTeams[i],
-            team2: groupTeams[j],
-            time: {
-              playing: duration * 2,
-              gap: gap + breakDuration,
-            },
-          });
-          const minWait =
-            Math.round((duration * 2 + gap + breakDuration) / 5) * 5;
-          timeOffset += minWait;
-          nextStart = addMinutes(nextStart, minWait);
-        }
-      }
+    Object.keys(T.categories).forEach(cat => {
+      console.log(T.categories[cat]);
     });
-*/
-    return matches;
   }
-  // Figure out who can play who and when
-  scheduleGroupStageFixtures() {}
-  calculateKnockoutFixtures(cup) {}
+
+  // 3. Generate group stage fixtures
+  orderGroupStageFixtures() {
+    // order by idealized order (i.e. if each schedule )\
+    const T = this.tournament;
+    T.fixtures = T.fixtures.sort((a, b) => {
+      return a.offset > b.offset ? 1 : b.offset > a.offset ? -1 : 0;
+    });
+    T.fixtures.forEach(fixture => {
+      fixture.matchId = this.nextFixture++;
+    })
+  }
+
 }
 
 module.exports = TournamentOrganize;
--- src/TournamentOrganize.spec.js
+++ src/TournamentOrganize.spec.js
@@ -5,7 +5,8 @@ const TournamentOrganize = require('./TournamentOrganize.class');
 const tournament_1 = TEST_DATA.tournaments["t1"];
 
 describe('TournamentOrganize', () => {
-  it.skip("can assign teams to groups", () => {
+
+  it("can assign teams to groups", () => {
     const cat = 'Mens'
     const T = new Tournament(tournament_1)
     const TO = new TournamentOrganize(T)
@@ -13,29 +14,23 @@ describe('TournamentOrganize', () => {
     TO.assignTeamsToGroups(cat)
     expect(T.categories[cat].groups).not.toBeFalsy()
   })
-  
-  it('Can schedule group fixtures', () => {
-    const T = new Tournament(tournament_1)
-    const TO = new TournamentOrganize(T);
-    T.clearFixtures();
-    // T.updateGroups();
-    expect(T.schedule.length).toBe(0);
-    const cat = 'Mens';
-    TO.assignTeamsToGroups(cat);
-    const matches = TO.calculateGroupStageFixtures(cat);
-    console.table(matches)
-  })
+
+
+
 })
 
 describe('End to end tournament organization', () => {
+
   it('completes the definition of a tournament from a minimal rules input', () => {
     const T = new Tournament(tournament_1);
     const TO = new TournamentOrganize(T);
     TO.generate();
+    expect(true).toBe(true)
     // expect teams to be placed in groups
     // expect multiple fixtures
     // expect group fixtures to be set out
     // expect knockout fixtures to be calculated also
     // expect schedule to be set for all games
   })
+
 })
