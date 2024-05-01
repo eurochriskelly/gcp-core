4c4b44e15238680496faa7d8c01e8efd6724ed69
Chris Kelly <eurochriskelly@gmail.com>
2024-05-01 23:59:09 +0200

    Create a single entry point for controlling the import interface for modules

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { 
+  ITournament, IPitch, IRules, IFixture, IBrackets, ICategory, IFixProps, IAvailabilty 
+} from "../types";
+
+// classes
+export * as Tournament from './Tournament.class';
+// libraries
+export * as validation from './validate';
+
+// test data
+export * as testdata from '../test/test-data';
